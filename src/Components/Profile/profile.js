2147164f50680ba87d3a2f7097fbe8bd13718247
dreamapaul <dreamapaulv@gmail.com>
2024-02-18 16:51:05 +0530

    added data from database to profile

--- src/Components/Profile/profile.js
+++ src/Components/Profile/profile.js
@@ -1,62 +1,96 @@
-import './profile.css';
-import { Box, Image, Avatar, Text, } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-const Profile = () => {
-    return (
-        <Box className='bg'>
-            <Box className='dashboard'>
-                <Image className="imageuserhome" src='/images/logo.svg' />
-                <Avatar className='user' image="/images/user-solid.svg" />
-                <Text className='name'>Name</Text>
-                <Text className='email'>abc@gmail.com</Text>
-                <Image className="linedash" src='/images/line_1.svg' />
-                <Image className="home" src="/images/home.svg" />
-                <Link className='hometxt' to={'/'}>Home</Link>
-                <Image className="profile" src="/images/profile.svg" />
-                <Link className='proftxt'>Profile</Link>
-                <Image className="help" src="/images/help.svg" />
-                <Link className='helptxt'>How to Create?</Link>
-                <Image className="category" src="/images/category.svg" />
-                <Link className='categtxt'>Categories</Link>
-                <Image className="logout" src="/images/logout.svg" />
-                <Link className='logtxt'>Log Out</Link>
-            </Box>
+import React from "react";
+import { Image, Text, Avatar } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import "./profile.css";
+import axios from 'axios';
+import { useState,useEffect } from "react";
+import { useCookies } from 'react-cookie';
 
-            <Box className="personal">
-                <Text className="head">Personal Information</Text>
-                <div className="rect1">
-                    <Text className="left">Name</Text>
-                    <Text className="col">:</Text>
-                    <Text className="right">ABCD</Text>
+const Profile = () => {
+  const [cookies] = useCookies(['login']);
+  const[user,setUser]=useState([]);
+   
+   useEffect(() =>{
+    const fetchBills = async () => {
+      try {
+        const response1 = await axios.post('http://localhost:3001/signup/' + cookies.name);
+        setUser(response1.data[0]);
+      } catch (error) {
+        console.error("Error fetching bills:", error);
+      }
+    };
+    fetchBills();
+   },[]);
+
+  return (
+    <div className="profile_page">
+      <div className="header1">
+        <Image className="header_logo" src="/images/logo.svg" />
+      </div>
+      <div className="line_div1">
+        <Image className="line" src="/images/line_1.svg" />
+      </div>
+      <div className="profile_body">
+      <div className="dashboard1">
+          <div className="dash_items1">
+            <Avatar className="user" image="/images/user-solid.svg" />
+            <div className="avatar_txt">
+              <Text className="name_txt">{user.username}</Text>
+              <Text className="email_txt">{user.email}</Text>
+            </div>
+          </div>
+          <div className="dash_items">
+            <Image className="dashboard_home" src="/images/home.svg" />
+            <Link className="home_txt" to={"/userhome"}>Home</Link>
+          </div>
+          <div className="dash_items">
+            <Image className="dashboard_profile" src="/images/profile.svg" />
+            <Link className="profile_txt" to={"/profile"}>Profile</Link>
+          </div>
+          <div className="dash_items">
+            <Image className="dashboard_help" src="/images/help.svg" />
+            <Link className="help_txt" to={"/help"}>How to Create?</Link>
+          </div>
+          <div className="dash_items">
+            <Image className="dashboard_category" src="/images/category.svg" />
+            <Link className="category_txt" to={"/categories"}>Categories</Link>
+          </div>
+          <div className="dash_items">
+            <Image className="back_dash" src="/images/logout.svg" />
+            <Link className="dash_txt" to={"/"}>Log Out</Link>
+          </div>
+        </div>
+
+
+        <div className="profile_container">
+          <div className="prof1">
+            <Text className="prof_title">Personal Information</Text>
+            <div className="rect">
+              <div className="rect1">
+                <Text className="left">Name</Text>
+                <Text className="col1">:</Text>
+                <Text className="right">{user.name}</Text>
+              </div>
+              <div className='rect1'>
+                   <Text className="left">Username</Text>
+                    <Text className="col2">:</Text>
+                     <Text className="right">{user.username}</Text>
                 </div>
-                <div className='rect2'>
-                    <Text className="left">Username</Text>
-                    <Text className="col">:</Text>
-                    <Text className="right">abcd</Text>
+                <div className='rect1'>
+                   <Text className="left">Password</Text>
+                    <Text className="col3">:</Text>
+                     <Text className="right">{user.password}</Text>
                 </div>
-                <div className='rect3'>
-                    <Text className="left">Password</Text>
-                    <Text className="col">:</Text>
-                    <Text className="right">123456</Text>
+                <div className='rect1'>
+                   <Text className="left">Email ID</Text>
+                    <Text className="col4">:</Text>
+                     <Text className="right">{user.email}</Text>
                 </div>
-                <div className='rect4'>
-                    <Text className="left">Email ID</Text>
-                    <Text className="col">:</Text>
-                    <Text className="right">abc@gmail.com</Text>
-                </div>
-            </Box>
-            <Box className='web'>
-                <Text className="head">Websites Created</Text>
-                <div className='wrect'>
-                    <Text className='left'>GetPass - An automated Bus Pass Purchasing System</Text>
-                </div>
-            </Box>
-            <Image className="line" src='/images/line_1.svg' />
-            <Image className="eclipse" src='/images/Eclipse_1.svg'></Image>
-            <Image className="bright_1" src='/images/bright_1.svg'></Image>
-            <Image className="eclipse_2" src='/images/Eclipse_2.svg'></Image>
-            <Image className="bright_2" src='/images/bright_2.svg'></Image>
-        </Box>
-    );
-}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Profile;
