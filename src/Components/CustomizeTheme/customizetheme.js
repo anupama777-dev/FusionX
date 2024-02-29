b2e09621ef1297870fedab9c7f78c3f95f898a29
Anupama Vidyasagar <anupama777vidya@gmail.com>
2024-02-29 17:19:09 +0530

    Store link added

--- src/Components/CustomizeTheme/customizetheme.js
+++ src/Components/CustomizeTheme/customizetheme.js
@@ -69,26 +69,34 @@ function CustomizeTheme() {
       </div>
       <div className="customize_body">
         <div className="dashboard2">
-          <div className="dash_items">
-            <Image className="dashboard_home" src="/images/home.svg" />
+        <div className="dash_items">
+            <Link to={"/userhome"}>
+              <Image className="dashboard_home" src="/images/home.svg" />
+            </Link>
             <Link className="home_txt" to={"/userhome"}>
               Home
             </Link>
           </div>
           <div className="dash_items">
-            <Image className="dashboard_profile" src="/images/profile.svg" />
+            <Link to={"/profile"}>
+              <Image className="dashboard_profile" src="/images/profile.svg" />
+            </Link>
             <Link className="profile_txt" to={"/profile"}>
               Profile
             </Link>
           </div>
           <div className="dash_items">
-            <Image className="dashboard_help" src="/images/help.svg" />
+            <Link to={"/help"}>
+              <Image className="dashboard_help" src="/images/help.svg" />
+            </Link>
             <Link className="help_txt" to={"/help"}>
               How to Create?
             </Link>
           </div>
           <div className="dash_items">
-            <Image className="back_dash" src="/images/logout.svg" />
+            <Link to={"/"} onClick={handleLogout}>
+              <Image className="back_dash" src="/images/logout.svg" />
+            </Link>
             <Link className="dash_txt" to={"/"} onClick={handleLogout}>
               Log Out
             </Link>
